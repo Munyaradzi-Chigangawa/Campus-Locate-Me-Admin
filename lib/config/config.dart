222aebd2f0e620a6500e7fd91580594e65df7957
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-08-09 08:03:43 +0200

    first commit

---
+++ lib/config/config.dart
@@ -0,0 +1,9 @@
+
+class Config{
+  final String appName = 'Campus Locate Me'; //app name
+  final String testerPassword = 'tester12345';  //testing password
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAAjdNcScs:APA91bFCj9L4PhN6oSbOTmoD4fIukpjmH9hiAmOw-mkbR4xOwKKLSyulQVlpXPaeE83OlgQTDKyPW3N4NB7spbsZiV2NlypW3QxxL6Sy9-_dgckOgI_RMMiKkr9OMIsjQa7owxWIS1aG';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
